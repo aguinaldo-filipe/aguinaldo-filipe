0e3488e3a4e54a602512129607705903056a59f4
aguinaldo-filipe <naldolipe@hotmail.com>
2024-07-20 02:44:17 +0200

    adição de novas variaveis
    Adicionei varáveis de nome, idade e género

--- Novo git.py
+++ Novo git.py
@@ -1,3 +1,5 @@
-nome = "Edson"
+nome = input("Qual é o nome do paciente? ")
+idade = input("Qual é o idade do paciente? ")
+genero = input("Qual é o genero do paciente? ")
 
-print(f"Nome do paciente é {nome}")
+print(f"\nNome do paciente é {nome}, tem {idade} anos e é do genero {genero}")
