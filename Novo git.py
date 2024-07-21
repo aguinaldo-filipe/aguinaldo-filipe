ccbb4e3dc2ef0d471a49b4842cce861c5f09ea2e
aguinaldo-filipe <naldolipe@hotmail.com>
2024-07-21 23:57:34 +0200

    Criação da Primeira lista
    \n para
    
    \n saltar uma linha
    pRINT (f é para adicionar palavras
    print sem NORMAL sem f Para variaveis sem texto
    printe e Input sempre parenteses curvo() abrangindotodas as funções
    Strings usa-se virgulas altas ""
    
    .

--- Novo git.py
+++ Novo git.py
@@ -1,5 +1,11 @@
-nome = input("Qual é o nome do paciente? ")
-idade = input("Qual é o idade do paciente? ")
-genero = input("Qual é o genero do paciente? ")
+#nome = input("Qual é o nome do paciente? ")
+#idade = input("Qual é o idade do paciente? ")
+#genero = input("Qual é o genero do paciente? ")
 
-print(f"\nNome do paciente é {nome}, tem {idade} anos e é do genero {genero}")
+#print(f"\nNome do paciente é {nome}, tem {idade} anos e é do genero {genero}")
+# Criação de uma Lista
+pacientes = ["Roberto", "Julia", "Ana", "Alex"]
+
+#print(f"\n pacientes")
+print(pacientes[0],pacientes[2]) 
+print(f"{pacientes[0]} e a {pacientes[2]}")
