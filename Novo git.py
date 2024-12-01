83c0e807a20ba5a12f17e9906562f965a5ef9aa1
aguinaldo-filipe <naldolipe@hotmail.com>
2024-12-01 21:46:14 +0100

    Update Novo git.py

--- Novo git.py
+++ Novo git.py
@@ -1,6 +1,6 @@
-#nome = input("Qual é o nome do paciente? ")
-#idade = input("Qual é o idade do paciente? ")
-#genero = input("Qual é o genero do paciente? ")
+nome = input("Qual é o nome do paciente? ")
+idade = input("Qual é o idade do paciente? ")
+genero = input("Qual é o genero do paciente? ")
 
 #print(f"\nNome do paciente é {nome}, tem {idade} anos e é do genero {genero}")
 # Criação de uma Lista
@@ -14,3 +14,23 @@ a=5; b=5; c=a+b;
 print(a),print(c)
 print(c),print(b)
 print(b),print(a)
+i = 2
+f = 2.75
+c = 2 + 3j
+print(i),print(c)
+print(f),print(i)
+print(c),print(f)
+# deletar pacientes na lista
+pacientes.remove("Anacleta")
+
+pacientes.append("Aguinaldo")
+print(pacientes)
+
+# Dicionário
+dict = {"name":"Aguinalda", 
+        "surname":"Filipe de Morais", 
+        "age":1, 
+        "sex": "F"}
+print(dict)
+# nome = input("Qual é o nome do paciente? ")
+print(f)
